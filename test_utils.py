25f2d7a7a3bb88e52e42937daf23db6e05902a16
Richa-2303 <chanderricha77@gmail.com>
2023-09-23 13:14:19 +0530

    corrected two test cases bugs

--- test_utils.py
+++ test_utils.py
@@ -4,7 +4,7 @@ def test_for_hyper_param_combinations_count():
     gamma_ranges=[0.001,0.01,0.1,1,10,100]
     C_ranges=[0.1,1,2,5,10]
     list_of_all_param_combination = [dict(zip(param_type, values)) for values in itertools.product(gamma_ranges, C_ranges)]
-    assert len(list_of_all_param_combination) == len(gamma_ranges)*list(C_ranges)
+    assert len(list_of_all_param_combination) == len(gamma_ranges)*len(C_ranges)
 
 def test_for_hyper_param_combinations_values():
     param_type=['gamma','C']
@@ -13,4 +13,4 @@ def test_for_hyper_param_combinations_values():
     list_of_all_param_combination = [dict(zip(param_type, values)) for values in itertools.product(gamma_ranges, C_ranges)]
     expected_param_combo1={'gamma':0.001,'C':1}
     expected_param_combo2={'gamma':0.01,'C':1}
-    assert len(list_of_all_param_combination) == len(gamma_ranges)*list(C_ranges)
+    assert (expected_param_combo1 in list_of_all_param_combination) and (expected_param_combo2 in list_of_all_param_combination)
